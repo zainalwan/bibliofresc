1d755b2311c4193e694ace112a6f0c532222c05d
zainalwan <zainalwan4@gmail.com>
2021-04-30 19:46:37 +0700

    Create navbar shared view.

---
+++ resources/views/shared/navbar.blade.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-transparent">
+    <div class="container-lg">
+        <a class="navbar-brand fs-3" href="/">Bibliofresc</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link me-4" aria-current="page" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link me-4" href="/search">Search</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/about">About</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
