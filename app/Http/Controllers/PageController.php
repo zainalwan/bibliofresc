001e01d77ffbb0288b379ea5e13d85dd8a769eb4
zainalwan <zainalwan4@gmail.com>
2021-04-30 19:45:14 +0700

    Create PageController.php

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+
+class PageController extends Controller
+{
+    public function index()
+    {
+        $json_string = file_get_contents
+            ('http://openlibrary.org/search.json?q=mathematics');
+        $books = json_decode($json_string, true)['docs'];
+        foreach($books as $i => $book)
+        {
+            if(!isset($book['cover_i']) || $book['cover_i'] < 0)
+            {
+                Arr::pull($books, $i);
+            }
+        }
+        $datas = [
+            'books' => $books
+        ];
+        return view('pages.landing_page', $datas);
+    }
+}
