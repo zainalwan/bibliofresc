d14fbf14d588bdaceedfbb35db7622dcffd855b3
zainalwan <zainalwan4@gmail.com>
2021-04-30 21:48:31 +0700

    Create about page child view

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.default')
+
+@section('content')
+    <div class="container container-min">
+        <div class="row py-5">
+            <div class="col-md-10 mx-auto">
+                <h2 class="mb-3">About</h2>
+                <p>Bibliofresc is a web app that provide features for search and displaying details of the books. You could get any information including the cover, author, publisher, publish year, etc. Thanks to <a href="https://openlibrary.org">openlibrary</a> for providing REST API server.</p>
+            </div>
+        </div>
+    </div>
+@endsection
