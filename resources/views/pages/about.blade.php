b98c4a58d03b0c6c14ba757be13406c7ad972ba5
zainalwan <zainalwan4@gmail.com>
2021-05-01 12:26:08 +0700

    Change 'openlibrary' to 'Open Library'

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -5,7 +5,7 @@
         <div class="row py-5">
             <div class="col-md-10 mx-auto">
                 <h2 class="mb-3">{{ $title }}</h2>
-                <p>Bibliofresc is a web app that provide features for search and displaying details of the books. You could get any information including the cover, author, publisher, publish year, etc. Thanks to <a href="https://openlibrary.org">openlibrary</a> for providing REST API server.</p>
+                <p>Bibliofresc is a web app that provide features for search and displaying details of the books. You could get any information including the cover, author, publisher, publish year, etc. Thanks to <a href="https://openlibrary.org">Open Library</a> for providing REST API server.</p>
             </div>
         </div>
     </div>
