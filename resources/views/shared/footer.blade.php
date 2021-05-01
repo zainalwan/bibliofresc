6dad1e844016d0538c07adfbf97ba09a5385fdfc
zainalwan <zainalwan4@gmail.com>
2021-05-01 07:10:03 +0700

    Insert heart logo inside footer.

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -1,1 +1,7 @@
-<p class="fs-6 text-center mb-0 py-3">Copyright 2021 by Zain Alwan Wima Irfani</p>
+<p class="fs-6 text-center mb-0 py-3">
+    Made with
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+    </svg>
+    by Zain Alwan Wima Irfani
+</p>
